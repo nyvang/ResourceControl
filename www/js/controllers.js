d1cc86f94cbc6649743acaa8e3056d0a97165142
Nicolaj Nyvang <nicolajnyvang@gmail.com>
2016-02-28 22:37:24 +0100

    Small cleanup
    A little refactoring for better readabillity

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,88 +12,90 @@ NyvangApp.controller('page2Ctrl', function($scope) {
 })
    
 .controller('addEntryCtrl', ['$scope', '$ionicModal', '$ionicLoading', 'dataService', '$window',
-      function($scope, $ionicModal, $ionicLoading, dataService, $win) {
+    function($scope, $ionicModal, $ionicLoading, dataService, $win) {
 
-      $scope.electrityForm = {};
-      $scope.electrityForm.month = "";
-      $scope.electrityForm.kwh = "";
-      $scope.electrityForm.price = "";
-      $scope.electrityForm.modalTitle = "Add new entry";
+    $scope.electrityForm = {};
+    $scope.electrityForm.month = "";
+    $scope.electrityForm.kwh = "";
+    $scope.electrityForm.price = "";
+    $scope.electrityForm.modalTitle = "Add new entry";
 
-      var modalOptions = {
-           scope: $scope,
-            animation: 'slide-in-up',
-            focusFirstInput: true,
-            backdropClickToClose: true,
-            hardwareBackButtonClose: true
-      }
+    var modalOptions = {
+        scope: $scope,
+        animation: 'slide-in-up',
+        focusFirstInput: true,
+        backdropClickToClose: true,
+        hardwareBackButtonClose: true
+    }
 
-      $ionicModal.fromTemplateUrl('electricity-modal.html', modalOptions)
-      .then(function(modal) {
-            $scope.modal = modal;
-      });
-      $scope.openModal = function() {
-            $scope.modal.show();
-      };
-      $scope.closeModal = function() {
-            $scope.modal.hide();
-      };
-      //Cleanup the modal when we're done with it!
-      $scope.$on('$destroy', function() {
-            $scope.modal.remove();
-      });
-      // Execute action on hide modal
-      $scope.$on('modal.hidden', function() {
-            //$scope.modal.remove();
-      });
+    const FAKE_SAVE_DELAY = 500;
 
-      
-      $scope.electrityForm.submit = function(item, event) {
-            $ionicLoading.show({template: 'Saving...'});
-            console.log("form submitted");
-            var data = {
-                  use: $scope.electrityForm.kwh,
-                  price: $scope.electrityForm.price
-            }
 
-            var m = new Date($scope.electrityForm.month).getMonth();
+    /* Form
+     ----------------------------------*/
 
-           // storageE.setItem(m, data)
-          $win.setTimeout(function(){
+    $scope.electrityForm.submit = function(item, event) {
+        $ionicLoading.show({template: 'Saving...'});
+        //------------------------------------------------- log //
+        console.log("form submitted");
+        var data = {
+              use: $scope.electrityForm.kwh,
+              price: $scope.electrityForm.price
+        }
+
+        var m = new Date($scope.electrityForm.month).getMonth();
+
+        // storageE.setItem(m, data)
+        $win.setTimeout(function(){
             dataService.set(m, data);
-            
+        
             if(dataService.get(m)){
+                  //------------------------------------------ log //
                   console.log("saved!")
                   $scope.electrityForm.reset();
             }
             $ionicLoading.hide();
+        }, FAKE_SAVE_DELAY);
+    }
 
-          }, 500);
-          
-            //DataService.data.set('username': 'ifedi', 'fullname': { firstname: 'Ifedi', lastname: 'Okonkwo'});
-            //data.set(m, 'Electricity': { use: $scope.electrityForm.kwh, price: $scope.electrityForm.price });
-            // storageE.setItem(m, $scope.electrityForm.kwh) 
-      }
+    $scope.electrityForm.reset = function() {
+        $scope.electrityForm.month = "";
+        $scope.electrityForm.kwh = "";
+        $scope.electrityForm.price = "";
+    }
 
-      $scope.electrityForm.reset = function() {
-            $scope.electrityForm.month = "";
-            $scope.electrityForm.kwh = "";
-            $scope.electrityForm.price = "";
-      }
+    $scope.electrityForm.close = function() {
+        if($scope.modal.isShown()) {
+            $scope.closeModal();
+        }
+    }
 
-      $scope.electrityForm.close = function() {
-             if($scope.modal.isShown()) {
-                  $scope.closeModal();
-            }
-      }
+    /* Modal
+     ----------------------------------*/
 
-}])
+    $ionicModal.fromTemplateUrl('electricity-modal.html', modalOptions)
+    .then(function(modal) {
+        $scope.modal = modal;
+    });
+    $scope.openModal = function() {
+        $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+    
+    // end addEntry Controller
+}]) 
    
 .controller('aboutCtrl', function($scope) {
 
 })
 
 .controller('mainCtrl', function($scope) {
-       
+
 });
  
