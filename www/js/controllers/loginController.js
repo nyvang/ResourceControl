b50da2a0cfe3248662233ac83c8d294f6ae023cb
Nicolaj Nyvang <nicolajnyvang@gmail.com>
2016-03-06 12:45:45 +0100

    Added resources
    Added splashscreen and icon.
    Started inplementation of the "login controller"

---
+++ www/js/controllers/loginController.js
@@ -0,0 +1,30 @@
+﻿/**
+ * LOGIN CONTROLLER 
+ */
+NyvangApp.controller('loginCtrl', function ($scope, $state) {
+
+    var authProvider = "basic";
+    var opts     = {'remember': true}; 
+
+    var credentials = {
+        'email'     : 'nn@smartpage.dk',
+        'password'  : '1234'
+    }
+
+    $scope.authSuccess = function () {
+        c("User logged in!");
+        $state.go('main');
+    };
+
+    $scope.authFaliure = function (errors) {
+        c("User rejected:");
+        for (var err in errors) {
+            c(err);
+        }
+    }
+
+    $scope.login = function (provider) {
+        Ionic.Auth.login(authProvider, opts, credentials)
+            .then(authSuccess, authFaliure);
+    };
+})
